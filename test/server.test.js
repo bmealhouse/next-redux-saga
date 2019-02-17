ef7d6cfa7faf7328b42511a95d47595945fbeeb2
Brent Mealhouse <bmealhouse@gmail.com>
2019-02-17 14:44:46 -0600

    Fix failing test, the result looks much more correct now

--- test/server.test.js
+++ test/server.test.js
@@ -8,7 +8,11 @@ import SyncGetInitialProps from './components/sync-get-initial-props'
 import configureStore from './store/configure-store'
 import createSnapshot from './utils/create-snapshot'
 import getInitialProps from './utils/get-initial-props'
-import {STATIC_PROP_TEXT, SYNC_REDUX_PROP_TEXT} from './constants'
+import {
+  ASYNC_REDUX_SAGA_PROP_TEXT,
+  STATIC_PROP_TEXT,
+  SYNC_REDUX_PROP_TEXT,
+} from './constants'
 
 test('Wrapped component passes along React props', () => {
   const WrappedComponent = withRedux(configureStore)(
@@ -49,7 +53,10 @@ test('Wrapped component awaits asynchronous getInitialProps', async () => {
 
   const props = await getInitialProps(WrappedComponent)
   expect(props.isServer).toBeTruthy()
-  expect(props.initialState).toEqual({syncReduxProp: SYNC_REDUX_PROP_TEXT})
+  expect(props.initialState).toEqual({
+    syncReduxProp: SYNC_REDUX_PROP_TEXT,
+    asyncReduxSagaProp: ASYNC_REDUX_SAGA_PROP_TEXT,
+  })
   expect(props.initialProps).toEqual({staticProp: STATIC_PROP_TEXT})
 
   createSnapshot(WrappedComponent, props)
